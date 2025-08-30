2fb8223674f7c180fc5821f53d9e1a6d7882de8f
Shreyansh Srivastava <123120269+shreyansh80321@users.noreply.github.com>
2025-08-30 13:13:35 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://sibersuper1122_db_user:shreyanshnancy8032180321@cluster0.40bqant.mongodb.net/GroceryBasket"
-    )
+    .connect()
     .then(() => console.log("DB Connected"));
 
 }
+
